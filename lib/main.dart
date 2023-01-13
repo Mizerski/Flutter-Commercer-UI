9e4f79a4336b2295ea73d4f968ceb37d6436da41
Wesley <mizerskimachado@hotmail.com>
2023-01-13 10:55:36 -0300

    final fix

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce/ShopPage/ShopPage.dart';
+import 'package:e_commerce/SplashPage/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
